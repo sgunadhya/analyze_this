fcad49ee1734b3e9b0c6dac48233d965fb5af3f9
Sushant Srivastava <sushant.srivastava@concur.com>
2017-06-11 21:22:37 +0530

    Moving files int resources

--- src/analyze_thesis/core.clj
+++ src/analyze_thesis/core.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [clojure.java.io :as io]))
 
-(defn all-pdf-files "List all the pdf files in the directory" [directory]
+(defn- all-pdf-files "List all the pdf files in the directory" [directory]
   (filter (fn [x]
             (.contains (.getName x) ".pdf")))
   (file-seq (io/file directory)))
